c7625ae79fc0cb81e59344d44e5652d9effd7467
gasparhdz <74631249+gasparhdz@users.noreply.github.com>
2021-04-12 21:33:37 -0300

    agregamos js

---
+++ boletines/javascript/main.js
@@ -0,0 +1,29 @@
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(link);
+});
+*/
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+
+            location.href = "file:///C:/Users/Gaspar%20Hernandez/Documents/GitHub/gasparhdz.github.com/boletines/index.html";
+
+        },1000);
+
+        return false;
+    });
+});
